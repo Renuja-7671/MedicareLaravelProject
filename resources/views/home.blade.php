b100d1617b6a17f53b50eb405a8aba753b91cbf3
Renuja-7671 <renujajanith7671@gmail.com>
2024-12-31 16:13:41 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($medicines as $medicine)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $medicine->name }}</td>
+                                <td>{{ $medicine->quantity }}</td>
+
+
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
+    </div>
+</div>
+@endsection
